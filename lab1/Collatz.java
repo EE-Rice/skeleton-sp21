4d0b30b92224a01c8183a7c127c234cb91cebb46
EE-Rice <kc202206@163.com>
2026-03-03 21:45:08 +0800

    Update Collatz.java

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -1,16 +1,19 @@
 /** Class that prints the Collatz sequence starting from a given number.
- *  @author YOUR NAME HERE
+ *  @author EERice
  */
 public class Collatz {
 
     /** Buggy implementation of nextNumber! */
     public static int nextNumber(int n) {
-        if (n  == 128) {
+        /**
+         * 如果 n 为偶数，则下一个数是 n/2。如果 n 为奇数，则下一个数是 3n + 1。如果 n 为 1，则数列结束。
+         */
+        if (n == 1) {
             return 1;
-        } else if (n == 5) {
-            return 3 * n + 1;
+        } else if (n % 2 == 0) {
+            return n / 2;
         } else {
-            return n * 2;
+            return 3 * n + 1;
         }
     }
 
